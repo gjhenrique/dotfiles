eae438b30d992e6d2732d8291bc7bac8b1fd29e0
Guilherme Henrique <gjhenrique@gmail.com>
2017-12-19 12:23:40 -0200

    Dont hardcode the home path

--- files/.fzf.zsh
+++ files/.fzf.zsh
@@ -1,14 +1,14 @@
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == */home/guilherme/.fzf/bin* ]]; then
-  export PATH="$PATH:/home/guilherme/.fzf/bin"
+  export PATH="$PATH:$HOME/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/guilherme/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/guilherme/.fzf/shell/key-bindings.zsh"
+source "$HOME/.fzf/shell/key-bindings.zsh"
 
