b4a9d1c4bec80a0949d3289775f5a5113152e796
Guilherme Henrique <gjhenrique@gmail.com>
2020-05-21 15:07:28 +0200

    Clean up binary files

--- scripts/bin/import-bookmark.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import os
-import buku
-
-location = '~/.config/chromium/Default/Bookmarks'
-bookmarks_database = os.path.expanduser(location)
-db = buku.BukuDb()
-db.load_chrome_database(bookmarks_database, buku.gen_auto_tag(), True)
