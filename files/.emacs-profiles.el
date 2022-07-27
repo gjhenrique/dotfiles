8bfe9325f5088f150cdd901d7e900cea8749843e
Guilherme Henrique <gjhenrique@gmail.com>
2022-07-27 17:19:18 +0200

    Don't use different profiles. Stick with doom

--- files/.emacs-profiles.el
+++
@@ -1,4 +0,0 @@
-(("default" . ((user-emacs-directory . "~/Projects/elisp/doom-emacs")))
- ("personal" . ((user-emacs-directory . "~/Projects/mine/zezin.emacs")))
- ("spacemacs" . ((user-emacs-directory . "~/Projects/elisp/spacemacs")))
- ("vanilla" . ((user-emacs-directory . "~/Projects/mine/zezin.emacs/vanilla"))))
