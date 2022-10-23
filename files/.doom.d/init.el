77e4fd26a06a762a747ebd4611784cc48049207f
Guilherme Henrique <gjhenrique@gmail.com>
2022-10-23 21:19:51 +0200

    Add docker and clojure mode

--- files/.doom.d/init.el
+++ files/.doom.d/init.el
@@ -85,7 +85,7 @@
        ansible
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
-       ;; docker
+       docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
@@ -107,7 +107,7 @@
        :lang
        ;;agda              ; types of types of types of types...
        (cc +lsp)               ; C/C++/Obj-C madness
-       ;;clojure           ; java with a lisp
+       clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
