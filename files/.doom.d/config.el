0acd591d1ca2e16c756c0d65e7d3585458e357ab
Guilherme Henrique <me@gjhenrique.com>
2023-06-19 09:56:30 +0200

    Some emacs configs

--- files/.doom.d/config.el
+++ files/.doom.d/config.el
@@ -20,7 +20,7 @@
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
 
-(setq doom-font (font-spec :family "Source Code Pro" :size 15))
+(setq doom-font (font-spec :family "Source Code Pro" :size 13))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -242,6 +242,11 @@
 (setq-hook! 'typescript-mode-hook +format-with-lsp nil)
 (setq-hook! 'js2-mode-hook +format-with-lsp nil)
 (setq-hook! 'typescript-tsx-mode-hook +format-with-lsp nil)
+
+(eval-after-load 'browse-at-remote
+  '(progn
+     (add-to-list 'browse-at-remote-remote-type-regexps
+                  '(:host "^gitlab\\.freedesktop\\.org" :type "gitlab"))))
 
 (after! emacs-everywhere
   (defun zezin-jira-page-p ()
