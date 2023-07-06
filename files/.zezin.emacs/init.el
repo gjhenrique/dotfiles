e74e54b96cb972c43d92970a7acb73b3de354c3d
Guilherme Henrique <me@gjhenrique.com>
2023-07-06 18:18:06 +0200

    More emacs goodness

--- files/.zezin.emacs/init.el
+++ files/.zezin.emacs/init.el
@@ -34,17 +34,26 @@
 ;; Evil
 (straight-use-package 'evil)
 (straight-use-package 'evil-nerd-commenter)
+(straight-use-package 'evil-collection)
 
 (use-package evil
   :init
   (progn
+    (setq evil-want-keybinding nil)
     (setq evil-want-C-u-scroll t)
     (setq evil-want-C-i-jump nil))
   :config
   (evil-mode 1))
 
+
 (use-package evil-nerd-commenter
   :commands (evilnc-comment-or-uncomment-lines evilnc-comment-or-uncomment-paragraphs))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (setq evil-collection-mode-list '(magit dired))
+  (evil-collection-init))
 
 ;; ivy/counsel/swiper
 (straight-use-package 'ivy)
@@ -66,5 +75,111 @@
   :config
   (ivy-rich-mode))
 
+
+(straight-use-package 'envrc)
+(use-package envrc
+  :config
+  (envrc-global-mode +1))
+
+;; python
+(push '(python-mode . python-ts-mode) major-mode-remap-alist)
+
+(with-eval-after-load 'eglot
+  (push '(python-ts-mode "pylsp" "--verbose" "--log-file=/tmp/server.log") eglot-server-programs))
+
+(defun zezin-split-window-below-and-focus ()
+  (interactive)
+  (split-window-below)
+  (windmove-down))
+
+(defun zezin-split-window-right-and-focus ()
+  (interactive)
+  (split-window-right)
+  (windmove-right))
+
+;; git
+;;
+(straight-use-package 'magit)
+(straight-use-package 'git-timemachine)
+(straight-use-package 'browse-at-remote)
+(straight-use-package 'git-gutter)
+
+(use-package magit
+  :commands magit-file-delete)
+
+(use-package git-timemachine
+  :commands git-timemachine)
+
+(use-package git-timemachine
+  :commands browse-at-remote)
+
+(use-package git-gutter
+  :hook ((prog-mode . git-gutter-mode)
+         (text-mode . git-gutter-mode)))
+
+;; code editing
+(straight-use-package 'dumb-jump)
+(use-package dumb-jump
+  :commands dumb-jump-go
+  :config
+  (setq dumb-jump-selector 'ivy
+        dumb-jump-aggressive nil))
+
+(cl-defun +region-or-symbol (&optional initial-text)
+  (or initial-text
+      (if (region-active-p)
+          (buffer-substring-no-properties
+           (region-beginning) (region-end))
+        (thing-at-point 'symbol))))
+
+(defun +counsel-find-read-dir ()
+  (interactive)
+  (let ((dir (file-name-directory (read-file-name "Choose directory: "))))
+    (counsel-fzf nil dir)))
+
+(defun +counsel-rg-directory (dir &optional initial-text)
+  (interactive)
+  (let ((text (or initial-text (+region-or-symbol) "")))
+    (counsel-rg text dir)))
+
+(defun +counsel-rg-region-or-symbol-read-dir ()
+  (interactive)
+  (let ((dir (file-name-directory (read-file-name "rg in directory: "))))
+    (+counsel-rg-directory dir)))
+
+(defun +counsel-rg-project ()
+  (interactive)
+  (let ((dir (or (cdr (cdr (project-current))) default-directory)))
+    (message dir)
+    (+counsel-rg-directory dir)))
+
+;; keybindings
+(with-eval-after-load 'evil
+  (progn
+    (evil-set-leader 'normal (kbd "SPC"))
+
+    (evil-define-key 'normal 'global (kbd "<leader>.") 'find-file)
+
+    (evil-define-key 'normal 'global (kbd "<leader>ww") 'evil-window-next)
+    (evil-define-key 'normal 'global (kbd "<leader>wd") 'evil-window-delete)
+    (evil-define-key 'normal 'global (kbd "<leader>jo") 'zezin-split-window-below-and-focus)
+    (evil-define-key 'normal 'global (kbd "<leader>js") 'zezin-split-window-right-and-focus)
+
+    (evil-define-key 'normal 'global (kbd "<leader>gg") 'magit-status)
+    (evil-define-key 'normal 'global (kbd "<leader>gt") 'git-timemachine)
+    (evil-define-key 'normal 'global (kbd "<leader>go") 'browse-at-remote)
+    (evil-define-key 'normal 'global (kbd "<leader>gk") 'browse-at-remote-kill)
+
+    (evil-define-key 'normal 'global (kbd "<leader>cd") 'xref-find-definitions)
+    (evil-define-key 'normal 'global (kbd "<leader>js") 'evilnc-comment-or-uncomment-lines)
+    (evil-define-key 'normal 'global (kbd "<leader>g,") 'dumb-jump-go)
+
+    (evil-define-key 'normal 'global (kbd "<leader>*") '+counsel-rg-project)
+    ))
+
+;; magit
+;;
+
+(find-file "/home/guilherme/Projects/python/Flexget/flexget/api/core/authentication.py")
 (provide 'init)
 ;;; init.el ends here
