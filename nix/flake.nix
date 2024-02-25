fde7fe89c913aa70a271a694412c8dc5631e330e
gjhenrique <me@gjhenrique.com>
2024-02-25 20:51:29 +0100

    Add custom home-manager

---
+++ nix/flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  inputs.home-manager = {
+    url = "github:nix-community/home-manager";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { home-manager, nixpkgs, ... }@inputs: let
+    system = "x86_64-linux";
+    username = "guilherme";
+
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+  in {
+    homeConfigurations = {
+      guilherme = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
+      };
+    };
+
+  };
+}
