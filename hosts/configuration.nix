219d6f71439c835d6e62855707645e9763993d22
gjhenrique <me@gjhenrique.com>
2025-09-13 07:44:41 +0200

    Revert "bump nixos"
    This reverts commit ad662e099d5025a8da30aa92026ac9d4919f9d0b.

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -45,6 +45,13 @@
     LC_TIME = "en_US.UTF-8";
   };
 
+  services.xserver.enable = true;
+  services.xserver = {
+    xkbOptions = "caps:escape";
+  };
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -211,8 +218,7 @@
 
       # Waybar
       font-awesome
-      pkgs.nerd-fonts.droid-sans-mono
-      # (nerdfonts.override {fonts = ["NerdFontsSymbolsOnly"];})
+      (nerdfonts.override {fonts = ["NerdFontsSymbolsOnly"];})
 
       noto-fonts
       noto-fonts-emoji
