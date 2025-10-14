a390a7e60c117962d90f534f043aaf38eebe4569
gjhenrique <me@gjhenrique.com>
2025-10-14 15:56:35 +0200

    add backrest ssh key

--- hosts/rpi/default.nix
+++ hosts/rpi/default.nix
@@ -7,9 +7,13 @@
   boot.loader.generic-extlinux-compatible.enable = true;
   users.users.guilherme.shell = pkgs.bash;
 
+  users.users.guilherme.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAOtJO7WF9lx0bpGrHUMDg6r3U2IoImwG+Afx1wmszEj backrest-backup-key"
+  ];
+
   services.ssh-auth.enable = true;
 
-    fileSystems."/mnt/external" = {
+  fileSystems."/mnt/external" = {
     device = "/dev/sda1";
     fsType = "ext4";
     options = [ "nofail" ];
