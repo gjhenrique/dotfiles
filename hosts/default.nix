ee453a20da0e6b7d41d28c5995534b72b74c4e2e
gjhenrique <me@gjhenrique.com>
2025-12-13 15:50:48 +0100

    remove migrated hosts to home-server

--- hosts/default.nix
+++ hosts/default.nix
@@ -3,40 +3,6 @@
   nixpkgs,
   dellNvidia,
 }: {
-  lisa = nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
-
-    specialArgs = {
-      pkgs = pkgsFor."x86_64-linux";
-
-      host = {
-        hostName = "lisa";
-      };
-    };
-    modules = [
-      ./lisa
-      ./configuration.nix
-      ../modules/scanning.nix
-      ../modules/ssh-auth.nix
-    ];
-  };
-  rpi = nixpkgs.lib.nixosSystem {
-    system = "aarch64-linux";
-
-    specialArgs = {
-
-      pkgs = pkgsFor."aarch64-linux";
-
-      host = {
-        hostName = "rpi";
-      };
-    };
-    modules = [
-      ./rpi
-      ./configuration.nix
-      ../modules/ssh-auth.nix
-    ];
-  };
   dell = nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
 
@@ -53,11 +19,9 @@
       ./dell
       ./configuration.nix
       dellNvidia
-      ../modules/scanning.nix
       ../modules/desktop.nix
       ../modules/virtualization.nix
       ../modules/yubikey.nix
-      ../modules/scanning.nix
     ];
   };
 }
